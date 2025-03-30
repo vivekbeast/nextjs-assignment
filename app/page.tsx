3bad99217c778acefc4b69d40e75533dda77814c
Vivek Tarnallya <vivek.empedance@gmail.com>
2025-03-31 01:24:27 +0530

    third

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { useRouter } from "next/navigation";
 import React from "react";
+import { Suspense } from "react";
+
+
 export default function Home() {
   const router = useRouter();
 
@@ -9,10 +12,12 @@ export default function Home() {
   }
 
   return (
-    <div className=" h-auto flex flex-row justify-center items-center overflow-hidden">
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className=" h-auto flex flex-row justify-center items-center overflow-hidden">
       <div onClick={navigate} className=" bg-[#640D51] p-3 text-white font-semibold text-sm rounded-full cursor-pointer">
         Click Here / Use Sidebar to navigate
       </div>
     </div>
+    </Suspense>
   );
 }
